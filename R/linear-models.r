326b12adc7787f843ae1bf17aa962e2793928148
zyrr95 <zhang@YurendeMBP.wireless.yale.internal>
2018-09-18 13:16:24 -0400

    implement lm

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,6 +10,22 @@
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
-linear_model <- function(formula, data) {
-  lm(formula, data)
+linear_model <- function(formula, data) linear_model = function(formula,data){
+  res = all.vars(formula)[1]
+  if( all.vars(formula)[-1] == '.'){
+    var = colnames(data)[colnames(data)!= res]
+  }
+  else{
+    var = all.vars(formula)[-1]
+  }
+  X = model.matrix(as.formula(paste("~", paste(var, collapse = " + "))), data = data)
+  Y = data[,res]
+  #beta = solve((t(X) %*% X)) %*% t(X) %*% Y
+  beta = qr.solve(X,Y)
+  beta[beta == 0] = NA
+  fitted = X %*% beta
+  residuals = Y - fitted
+  result = list(coefficients = beta, residuals = residuals, fitted.values = fitted, rank = ncol(x), weights = NULL, df.residual = nrow(X) - ncol(X), call = call('lm',formula), terms = terms(x = formula, data = data),contrasts = NA, xlelves = NA, offset = NA, y = Y, x = X, model = formula, na.action = NA, qr = qr(X))
+  class(result) = 'lm'
+  return(result)
 }
