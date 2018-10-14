7eed4958d2c6b443020370245cbcf8712a7b1a8e
zyrr95 <zhang@YurendeMacBook-Pro.local>
2018-10-14 19:00:08 -0400

    modify vignettes

--- R/data-doc.r
+++ R/data-doc.r
@@ -6,16 +6,17 @@
 #' @rdname lm_patho
 #' @description In homework 1 we explore the effect of numerical instability
 #' on linear regressions. This data set gives us a basis for exploration.
-#' 
-#' 
+NULL
+
+#
 #'A testing data set for the second homework
 #'
 #' @name ridge_test
 #' @docType data
 #' @rdname ridge_test
 #' @description This is the testing dataset used to evaluate out of sample mse of the ridge function
-#' 
-#' 
+"ridge_test"
+
 #'A trainning data set for the second homework
 #'
 #' @name ridge_train
@@ -23,5 +24,5 @@
 #' @rdname ridge_train
 #' @description This is the trainning dataset used to train ridge regression model.
 #' 
-NULL
+"ridge_train"
 
