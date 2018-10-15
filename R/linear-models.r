811c8f576d7fb318eb3d845c1470c7bd7d290d01
zyrr95 <zhang@YurendeMacBook-Pro.local>
2018-10-15 16:02:59 -0400

    modify linear regression file

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,7 +5,7 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm
+#' @importFrom stats as.formula model.matrix terms
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
